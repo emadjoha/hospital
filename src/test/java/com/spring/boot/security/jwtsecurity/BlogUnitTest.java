11549eb3ddfaf767c311baa36b56be4aa1217b46
EmadelddinJ <emad1.2@hotmail.com>
2019-12-28 02:10:18 +0200

    table name

--- src/test/java/com/spring/boot/security/jwtsecurity/BlogUnitTest.java
+++ src/test/java/com/spring/boot/security/jwtsecurity/BlogUnitTest.java
@@ -1,43 +1,43 @@
-package com.spring.boot.security.jwtsecurity;
-
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-import com.spring.boot.security.jwtsecurity.controller.UserController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import org.springframework.test.web.servlet.MockMvc;
-
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BlogUnitTest {
-    MockMvc mockMvc;
-
-    @Autowired
-    UserController userController;
-    @Before
-    public void setUp(){
-        this.mockMvc = standaloneSetup(this.userController).build();
-    }
-
-    @Test
-    public void testUserSize() throws Exception {
-        mockMvc.perform(get("/auth/a").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.*", hasSize(5)));
-    }
-
-}
-
+//package com.spring.boot.security.jwtsecurity;
+//
+//
+//import static org.hamcrest.Matchers.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+//import com.spring.boot.security.jwtsecurity.controller.UserController;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class BlogUnitTest {
+//    MockMvc mockMvc;
+//
+//    @Autowired
+//    UserController userController;
+//    @Before
+//    public void setUp(){
+//        this.mockMvc = standaloneSetup(this.userController).build();
+//    }
+//
+//    @Test
+//    public void testUserSize() throws Exception {
+//        mockMvc.perform(get("/auth/a").contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.*", hasSize(5)));
+//    }
+//
+//}
+//
