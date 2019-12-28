d0229ea9afaa0d9916cb522380b84297a1f41bda
EmadelddinJ <emad1.2@hotmail.com>
2019-12-28 02:43:30 +0200

    table name

--- src/main/java/com/spring/boot/security/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/com/spring/boot/security/jwtsecurity/JwtSecurityApplication.java
@@ -2,10 +2,16 @@ package com.spring.boot.security.jwtsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JwtSecurityApplication {
+public class JwtSecurityApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JwtSecurityApplication .class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(JwtSecurityApplication.class, args);
     }
